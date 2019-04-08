141910c8d74b7044a7db5557cb87703190b0507f
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-04-08 10:49:05 -0400

    footer updated

--- client/src/components/layouts/Footer.js
+++ client/src/components/layouts/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
 
-  <footer className="bg-dark text-center text-white mt-5 p-4">
+  <footer className="bg-dark text-center text-white mt-5 p-4 fixed-bottom">
   Copyright &copy; {new Date().getFullYear()} Comp 3133
   </footer>  
   )
